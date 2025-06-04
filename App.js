7bec033298435df6ab65084945916e794159174d
phuchoclaptrinh <168012773+phuchoclaptrinh@users.noreply.github.com>
2025-06-05 02:20:15 +0700

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-import { Navigation } from "./src/AppNavigation/Navigation";
- function App() {
-    return (
-      <Navigation/>
-    );
- }
- export default App;
