854c13ae72abcddaee1913c86776595a8c917971
phuchoclaptrinh <168012773+phuchoclaptrinh@users.noreply.github.com>
2025-06-05 02:18:25 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import { Navigation } from "./src/AppNavigation/Navigation";
+ function App() {
+    return (
+      <Navigation/>
+    );
+ }
+ export default App;
